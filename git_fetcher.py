09418be1f8cdbeeeeb32e269fc3583d0b44b5842
pkoiralap <pkoiralap@gmail.com>
2021-12-23 09:33:40 -0600

    make code better to hide details

--- git_fetcher.py
+++ git_fetcher.py
@@ -6,8 +6,11 @@ from tqdm import tqdm
 from time import sleep
 
 token = "your_token_here"
-url1 = "https://api.github.com/orgs/NASA-IMPACT/repos?per_page=100&page=1"
-url2 = "https://api.github.com/orgs/NASA-IMPACT/repos?per_page=100&page=2"
+ORG_NAME = "your_org_name_here"
+ANY_REPO_NAME = "any_repo_name_in_your_org"
+
+url1 = f"https://api.github.com/orgs/{ORG_NAME}/repos?per_page=100&page=1"
+url2 = f"https://api.github.com/orgs/{ORG_NAME}/repos?per_page=100&page=2"
 
 headers = {
     'Authorization': 'token ' + token,
@@ -25,7 +28,7 @@ contributors_headers = {
     'Cookie': '_octo=GH1.1.506912823.1566269566; _ga=GA1.2.288181271.1566269567; _device_id=a61263cb656c2cdfbbe38638c6f8689c; user_session=34bY-inbfXS0hTAmkSw2gPZMXBdiHTBmFnho6AsMAa0xkNWc; __Host-user_session_same_site=34bY-inbfXS0hTAmkSw2gPZMXBdiHTBmFnho6AsMAa0xkNWc; dotcom_user=Pkoiralap; logged_in=yes; tz=America%2FChicago; has_recent_activity=1; _gh_sess=LHIBJlf2f5CdlQ7YAEWFtpF5HA8cMoUk4BUCN%2BULzafeFy3L1CmcX2hwYndmPV%2BoTclmPnnjZNLbxrvU9IsTH5SZbTCZbMQTfKycp7EY7jqDAkOEndyxw5iFC56ICbMU09imhiou3Ct8UDUO%2BHKm6c8%2FXKyuMwIGju0xSZJTGXhFhQFrjnnB6j4QUYL8%2FYo9FfPBJw8QWw%2FeWxul16NkR%2Bjfip7D0oAaHqK4eB95DU%2B0pHURV%2BxuLw9MiqulqdtA4%2Fv3Fg79OyYmYIHm2Z9UjrH4uiYZMA85G2LqtXvapfaO5VndZcgbH8sjRD5JjlIAR5SadDYS4o6qu3kDqVjEDrSoHc07a1ulUSbkzI8mC7x9W22kdfzxHqujPvChPdrBm9sBvIZzqHuXkI4%2FwfduXTY8G%2FExrFGJP9%2B%2Fsig1r%2Fh49YGPmkQsNFiMEfTH%2BhxqS3sADm7Lt3N%2B1ghZ%2Fc43a8GvDZxYJViG%2FkDtgQl9h1V%2Btb184s0gZQnkMTsMX4jp4KylKf6dmqBb6RNcsZnrdPKTa11KdlSDRMAzi%2FwuS726UhEjwi7GElBswsOy1WOqOxYmK9EnsnrNT0kTEU5qBKUB4el6Y6l5O4y1GHvBvffj6I7o%2Bj2HGOec40OfZlIewJKdJUtu%2Blfupd89AzNG2KPduoDvOvFM0x%2Br083BmZTw12GnRQEeGAy1T0LjzMrR1%2BSVIzHPTrcp8JP27%2Fgq9qN1PYxvkE8L4CQY7Q7iJdEnv%2FhDc4e1IM2fF3QCN4Q9lV17RKSY9YwUSET7RVNUWMjsx7ipEsjo52hqKbb9oWDQZionl%2BXEkNTnAIVOchJBS5VYEPQ581n%2F%2BNRjPUlSSNvrdK8K9Pln--coH1HADXQf11p9f0--sDn5KXxsm6EaUj9H0poGbg%3D%3D',
     "Host": 'github.com',
     "Pragma": 'no-cache',
-    "Referer": 'https://github.com/NASA-IMPACT/image_labeler/graphs/contributors',
+    "Referer": f'https://github.com/{ORG_NAME}/{ANY_REPO_NAME}/graphs/contributors',
     "sec-ch-ua": '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
     "sec-ch-ua-mobile": '?0',
     "Sec-Fetch-Dest": 'empty',
@@ -68,8 +71,8 @@ def save_contributors(continue_from=0, sleep_time=10):
         if repo["fork"]:
             continue
         try:
-            url = "https://github.com/NASA-IMPACT/{}/graphs/contributors-data".format(repo["name"])
-            contributors_headers["Referer"] = 'https://github.com/NASA-IMPACT/{}/graphs/contributors'.format(repo["name"])
+            url = f"https://github.com/{ORG_NAME}/{{}}/graphs/contributors-data".format(repo["name"])
+            contributors_headers["Referer"] = f'https://github.com/{ORG_NAME}/{{}}/graphs/contributors'.format(repo["name"])
 
             sleep(sleep_time)
 
